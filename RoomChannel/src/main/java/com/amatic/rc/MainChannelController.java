57baea78c98f19860276a16f4e222e6bccde1a5b
Jorg <jorge.amatro@gmail.com>
2013-02-11 12:41:16 +0000

    +

--- RoomChannel/src/main/java/com/amatic/rc/MainChannelController.java
+++ RoomChannel/src/main/java/com/amatic/rc/MainChannelController.java
@@ -1,13 +1,14 @@
 package com.amatic.rc;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,20 +17,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MainChannelController {
 
-    List<Integer> sessions = new ArrayList<Integer>();
-    
-    Logger logger = LogManager.getLogger(MainChannelController.class.getName());
-    @SuppressWarnings("unchecked")
-    @RequestMapping(value = { "/index", "/" }, method = { RequestMethod.GET,
-            RequestMethod.POST })
-    public String getPostScreen(ModelMap model, HttpServletRequest request,
-            HttpServletResponse response) throws IOException {
-        HttpSession session = request.getSession();
-        
-        logger.trace("Entering application.");
-        logger.error("checking");
+	List<Integer> sessions = new ArrayList<Integer>();
 
-        return "mainChannel";
-    }
+	Logger logger = Logger.getLogger(MainChannelController.class.getName());
+
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value = { "/index", "/" }, method = { RequestMethod.GET,
+			RequestMethod.POST })
+	public String maininit(ModelMap model, HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		HttpSession session = request.getSession();
+
+		logger.info("checking");
+
+		return "mainChannel";
+	}
 
 }
